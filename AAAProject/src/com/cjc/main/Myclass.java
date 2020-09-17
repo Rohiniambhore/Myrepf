f452145fe47fd65091901e8898ce657da531515b
Admin <Admin@DESKTOP-DK3J5AE>
2020-09-17 11:19:20 +0530

    Updated by me

--- AAAProject/src/com/cjc/main/Myclass.java
+++ AAAProject/src/com/cjc/main/Myclass.java
@@ -2,9 +2,16 @@ package com.cjc.main;
 
 public class Myclass {
 	
+	public void m1()
+	{
+		System.out.println("Called m1 method......");
+	}
+	
 	public static void main(String args[])
 	{
 		System.out.println("....Hiiiiiiii....");
+		Myclass m=new Myclass();
+		m.m1();
 	}
 
 }
